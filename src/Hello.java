d3c7b90f01bea1e705b0a4b5e436c947bcbc8437
liam1221 <101781384+liam1221@users.noreply.github.com>
2022-09-07 16:21:33 +0800

    first add files
    first add files

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * @author 86135
+ */
+//少用魔法量
+//杨辉三角可以用加法解决
+    /*明确返回值和参数
+    明确结束条件
+    确定单层逻辑
+    * */
+
+public class Hello {
+    public static void main(String[] args) {
+            System.out.println("Hello World");
+    }
+}
+
+
---
+++ src/com/test/Person.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @author 86135
+ */
+public class Person {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
